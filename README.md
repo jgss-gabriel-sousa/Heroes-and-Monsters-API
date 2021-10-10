9d155b055e34f40232b6e6bb61d4c112a873b649
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2021-10-09 22:39:50 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# DnD-Image-API
+ 
+This project is a prototype of an Image API for the game Dungeons and Dragons (D&D or DnD)
+
+Check on: https://dnd-image-api.herokuapp.com
+
+/docs to see the documentation
+
+
+The maximum size of the images is 500 KB and all are .png with transparent background
---
+++ main.py
@@ -0,0 +1,75 @@
+from fastapi import FastAPI, HTTPException
+import random
+import os
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+directories = [
+    "data/monster/",
+    "data/spell/",
+    "data/class/",
+    "data/skills/",
+    "data/general/",
+]
+
+
+@app.get("/")
+async def full_index():
+    files = {}
+
+    for i in directories:
+        dirFiles = os.listdir(i)
+        dirFiles = [j[:-5] for j in dirFiles] #without file format
+        dirFiles.sort()
+
+        files[i[5:-1]] = dirFiles #without images folder
+
+    return files
+
+
+@app.get("/query-{name}")
+async def return_data_by_name(name: str):
+    found = False
+    name = name.lower()
+    name += ".json"
+
+    for i in directories:
+        folderFiles = os.listdir(i)
+
+        if name in folderFiles:
+            path = i+name
+            found = True
+            break
+
+    if found:
+        return FileResponse(path)
+    else:
+        raise HTTPException(status_code=404, detail="404: Item not found")
+
+
+@app.get("/index-{Type}")
+async def returns_the_index_of_a_type(Type: str):
+    try:
+        print(Type)
+        dirFiles = os.listdir("data/"+Type)
+        dirFiles = [j[:-5] for j in dirFiles] #without file format
+        dirFiles.sort()
+        return dirFiles
+    except:
+        raise HTTPException(status_code=404, detail="404: Item not found")
+
+
+@app.get("/{Type}/id:{id_value}")
+async def return_data_by_id(Type: str, id_value: int):
+    path = "data/"+Type+"/"
+    try:
+        files = os.listdir(path)
+    except:
+        raise HTTPException(status_code=404, detail="404: Item not found")
+
+    if(id_value < len(files)):
+        path += files[id_value]
+        return FileResponse(path)
+    else:
+        raise HTTPException(status_code=404, detail="404: Item not found")
