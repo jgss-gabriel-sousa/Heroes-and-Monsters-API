49b6279a4872ce9e4a411e1e16b50a27ec0a1f47
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2021-10-10 01:05:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,19 @@ from fastapi import FastAPI, HTTPException
 import random
 import os
 from fastapi.responses import FileResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 directories = [
     "data/monster/",
