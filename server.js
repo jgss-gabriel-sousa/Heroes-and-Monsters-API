4ad14d69382684a2eb052b2e060d9d7fb7476ca8
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2023-03-11 12:33:32 -0300

    query search

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT;
 
+directories = [
+    "data/general/",
+    "data/monster/",
+    "data/spell/",
+    "data/weapons/",
+]
+
 async function readJSONFile(filePath){
     try{
         const data = await fs.promises.readFile(filePath);
@@ -15,6 +22,30 @@ async function readJSONFile(filePath){
         throw new Error(`Error reading file: ${filePath}`);
     }
 }
+
+app.get(`/`, (req, res) => {
+    res.status(200);
+});
+
+app.get('/query-:name', async (req, res) => {
+    const name = req.params.name.toLowerCase() + '.json';
+    let found = false;
+    
+    await directories.forEach(dir => {
+        const folderFiles = fs.readdirSync(dir);
+        
+        if(folderFiles.includes(name)) {
+            const filePath = path.join(__dirname, dir, name);
+            found = true;
+            res.sendFile(filePath);
+        }
+    });
+  
+    if(!found) {
+        console.error(err);
+        res.status(404).send(`File not found: ${filePath}`);
+    }
+});
 
 app.get(`/:dataType/:jsonFile`, async (req, res) => {
     const fileName = req.params.jsonFile + ".json";
